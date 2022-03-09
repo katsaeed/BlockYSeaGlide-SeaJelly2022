28649968bcc63413b2c11d8c28f53f55439bce3b
katsaeed <93231906+katsaeed@users.noreply.github.com>
2022-03-09 09:53:12 -0500

    Update BlockY_SeaGlide_LEDTest.ino

--- SeaGlide/BlockY_SeaGlide_LEDTest.ino
+++ SeaGlide/BlockY_SeaGlide_LEDTest.ino
@@ -8,7 +8,8 @@ static byte BLUE_LED = 5;
 static byte LED_BASE = 7;
 
 void setup() {
-  Serial.begin(9600); //need to change this for SD card
+  Serial.begin(9600); //serial monitor
+  Serial.begin(115200); //OpenLog
   //LED
   pinMode(RED_LED, OUTPUT);
   pinMode(GREEN_LED, OUTPUT);
