ae8d28fbf31748d96b523f9b6671ff71320c8ddc
katsaeed <93231906+katsaeed@users.noreply.github.com>
2022-03-08 21:07:09 -0500

    Add files via upload

---
+++ BlockY_SeaGlide_LEDTest.ino
@@ -0,0 +1,34 @@
+//Katherine Saeed
+//08 March 2022
+
+//pins
+static byte RED_LED = 9;
+static byte GREEN_LED = 6;
+static byte BLUE_LED = 5;
+static byte LED_BASE = 7;
+
+void setup() {
+  Serial.begin(9600); //need to change this for SD card
+  //LED
+  pinMode(RED_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
+  pinMode(BLUE_LED, OUTPUT);
+  pinMode(LED_BASE, OUTPUT);
+}
+
+void LEDFlash() {
+  digitalWrite(LED_BASE, HIGH);
+  delay(1000);
+  digitalWrite(LED_BASE, LOW);
+  digitalWrite(BLUE_LED, HIGH);
+  delay(1000);
+  digitalWrite(LED_BASE, LOW);
+  digitalWrite(GREEN_LED, HIGH);
+  delay(1000);
+  digitalWrite(LED_BASE, LOW);
+  digitalWrite(RED_LED, HIGH);
+}
+
+void loop() {
+  LEDFlash();
+}
