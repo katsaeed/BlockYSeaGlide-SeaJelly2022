d7e23d624a814fc29774865447d1f3b0a2fae812
katsaeed <93231906+katsaeed@users.noreply.github.com>
2022-03-09 10:03:25 -0500

    Update BlockY_SeaJelly_TempSensorTest.ino

--- SeaJelly/BlockY_SeaJelly_TempSensorTest.ino
+++ SeaJelly/BlockY_SeaJelly_TempSensorTest.ino
@@ -1,15 +1,15 @@
 //Katherine Saeed
 //08 March 2022
 
-const byte tempPin = A2 //temperature pin
+const byte tempPin = A2; //temperature pin
 
-void setUp() {
+void setup() {
   Serial.begin(115200); //for OpenLog
   Serial.begin(9600); //for serial monitor
 }
 
 void loop() {
-  temp = map(analogRead(tempPin), 0, 1023, -10, 125) * (9.0 / 5.0) + 32.0;
+  temp = map(analogRead(tempPin), 0, 1023, -10, 125) * (9.0 / 5.0) + 32.0; //we don't really understand this yet
   Serial.println(temp);
   delay(1000);
 }
